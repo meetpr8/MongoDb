1b5fd4d60d73f158a614f38d77b10b23815aa75d
Meet Prajapati <56256369+meetpr8@users.noreply.github.com>
2021-03-19 14:12:38 +0530

    Update CustomRepo.java

--- src/main/java/com/example/demo/api/repository/CustomRepo.java
+++ src/main/java/com/example/demo/api/repository/CustomRepo.java
@@ -3,14 +3,18 @@ package com.example.demo.api.repository;
 import com.example.demo.api.model.movies;
 import org.bson.Document;
 
+import java.util.HashMap;
 import java.util.List;
 
 public interface CustomRepo {
     public List<movies> getMoviesOfActor(String actor);
-
     public List<Document> getTop10ImdbDirectors();
 
     List<movies> getMoviesWithGreaterRuntime(int runtime);
 
     List<movies> getMoviesFromNumReviewsAndRuntime(int tomatoNumReviews, int runtime);
+
+    void updateTitle(String title, HashMap<String, String> body);
+
+    void removeMovie(String title);
 }

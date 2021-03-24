40d8870c521d82edad8e2e694d15d58ec036f15b
Meet Prajapati <56256369+meetpr8@users.noreply.github.com>
2021-03-24 19:39:58 +0530

    Update ViewerRating.java

--- src/main/java/com/example/demo/api/model/ViewerRating.java
+++ src/main/java/com/example/demo/api/model/ViewerRating.java
@@ -1,30 +1,12 @@
 package com.example.demo.api.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document @Getter @Setter
 public class ViewerRating {
     private double rating;
     private int numReviews;
-
-    public ViewerRating() {
-        super();
-    }
-
-    public double getRating() {
-        return rating;
-    }
-
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
-
-    public int getNumReviews() {
-        return numReviews;
-    }
-
-    public void setNumReviews(int numReviews) {
-        this.numReviews = numReviews;
-    }
 
 }

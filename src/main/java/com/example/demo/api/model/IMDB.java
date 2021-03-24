f84e54f935cd1cea3b630216c53601445b426d2e
Meet Prajapati <56256369+meetpr8@users.noreply.github.com>
2021-03-24 19:36:45 +0530

    Update IMDB.java

--- src/main/java/com/example/demo/api/model/IMDB.java
+++ src/main/java/com/example/demo/api/model/IMDB.java
@@ -1,38 +1,14 @@
 package com.example.demo.api.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document @Getter @Setter
 public class IMDB {
     private int id;
     private int votes;
     private double rating;
 
-    public IMDB() {
-        super();
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getVotes() {
-        return votes;
-    }
-
-    public void setVotes(int votes) {
-        this.votes = votes;
-    }
-
-    public double getRating() {
-        return rating;
-    }
-
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
 }
